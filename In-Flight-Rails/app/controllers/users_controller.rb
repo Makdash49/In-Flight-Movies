a6dfabf0be6ebed98b8870465fe1aa7215913593
Mark McQuillen <markdmcq@hotmail.com>
2015-07-24 16:23:53 -0700

    working

--- In-Flight-Rails/app/controllers/users_controller.rb
+++ In-Flight-Rails/app/controllers/users_controller.rb
@@ -2,18 +2,29 @@ class UsersController < ApplicationController
 
 	def new
 		@user = User.new
+		@flights = Flight.all
 	end
 
 	def create
 		@user = User.create(user_params)
+		@flights = Flight.all
 		@flight = Flight.where(number: @user.flight).first
 		render 'new'
 	end
 
 
 def update
+	p "*" * 100
+	p params
 	@user = User.find(params[:id])
-	@user.update_attributes(flight: params[:user][:flight] )
+	@flights = Flight.all
+
+	@user.flight = params[:user][:flight]
+	@user.drama = params[:drama]
+	@user.romance = params[:romance]
+	@user.comedy = params[:comedy]
+	@user.save
+
 	@flight = Flight.where(number: @user.flight).first
 	render 'new'
 end
