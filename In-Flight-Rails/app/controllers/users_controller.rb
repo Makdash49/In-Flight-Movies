dcfdec2cb039baae20baa2517a9975df2c3eca0c
Mark McQuillen <markdmcq@hotmail.com>
2015-07-27 14:30:32 -0700

    show best combination of films for length

--- In-Flight-Rails/app/controllers/users_controller.rb
+++ In-Flight-Rails/app/controllers/users_controller.rb
@@ -29,31 +29,89 @@ def update
 
 	if @user.aaction == true && @user.comedy == true && @user.romance == true
 		special = actions & comedies & romances
-		@first_recom = special.select {|movie| movie.minutes < @user.flight.minutes }
+		@recom = special.select {|movie| movie.minutes < @user.flight.minutes }
+
+		# @recom is an array of movies that each fit the flight time.  I need to try every
+		# combination of those movies and add up the minutes and find the combination that is closest to 
+		# the flight time without going over.
+
+
+		@recom.each do |movie|
+			p movie.title
+			p movie.minutes
+		end
+
+		def get_combos(array)
+			n = array.length - 1
+			final_array = []
+			while n > 0
+				final_array += array.combination(n).to_a
+				n -= 1
+			end
+			final_array
+		end
+
+		array = @recom
+		p get_combos(array)
+
+		def add_array(array_of_arrays)
+			p array_of_arrays
+			index_of_winner = nil
+			top_sum = 0
+			array_of_arrays.each_with_index do |array, index|
+			sum = 0
+				array.each do |movie|
+					sum += movie.minutes
+				end
+
+				if sum > top_sum && sum < @user.flight.minutes
+					top_sum = sum
+					index_of_winner = index
+				end
+			end
+			p "*" * 100
+			p index_of_winner
+			p top_sum
+			@movie_fit = array_of_arrays[index_of_winner]
+		end
+
+		add_array(get_combos(array))
+
+
+
+
+
+
+
+
+
+
+
+
 
 	elsif @user.aaction == true && @user.comedy == true && @user.romance == nil
 		special = actions - romances & comedies
-		@first_recom = special.select {|movie| movie.minutes < @user.flight.minutes }
+		@recom = special.select {|movie| movie.minutes < @user.flight.minutes }
 
 	elsif @user.aaction == nil && @user.comedy == true && @user.romance == true
 		special = comedies - actions & romances
-		@first_recom = special.select {|movie| movie.minutes < @user.flight.minutes }
+		@recom = special.select {|movie| movie.minutes < @user.flight.minutes }
 
 	elsif @user.aaction == true && @user.comedy == nil && @user.romance == true
 		special = actions - comedies & romances
-		@first_recom = special.select {|movie| movie.minutes < @user.flight.minutes }
+		@recom = special.select {|movie| movie.minutes < @user.flight.minutes }
 
 	elsif @user.aaction == true && @user.comedy == nil && @user.romance == nil
 		special = actions - comedies - romances
-		@first_recom = special.select {|movie| movie.minutes < @user.flight.minutes }
+		@recom = special.select {|movie| movie.minutes < @user.flight.minutes }
 
 	elsif @user.aaction == nil && @user.comedy == true && @user.romance == nil
 		special = comedies - actions - romances
-		@first_recom = special.select {|movie| movie.minutes < @user.flight.minutes }
+		@recom = special.select {|movie| movie.minutes < @user.flight.minutes }
 
 	elsif @user.aaction == nil && @user.comedy == nil && @user.romance == true
 		special = romances - actions - comedies
-		@first_recom = special.select {|movie| movie.minutes < @user.flight.minutes }
+		@recom = special.select {|movie| movie.minutes < @user.flight.minutes }
 
 	end
 
