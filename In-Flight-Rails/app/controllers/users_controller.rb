157d5fd3c49f5a0321a6d9bb05154f5533c110d6
Mark McQuillen <markdmcq@hotmail.com>
2015-07-27 14:47:13 -0700

    refactor methods in controller

--- In-Flight-Rails/app/controllers/users_controller.rb
+++ In-Flight-Rails/app/controllers/users_controller.rb
@@ -14,33 +14,6 @@ class UsersController < ApplicationController
 
 
 def update
-	@user = User.find(params[:id])
-	@flights = Flight.all
-
-	@user.flight_id = params[:user][:flight_id]
-	@user.aaction = params[:aaction]
-	@user.romance = params[:romance]
-	@user.comedy = params[:comedy]
-	@user.save
-
-	actions = Genre.find(1).movies
-	comedies = Genre.find(2).movies
-	romances = Genre.find(3).movies
-
-	if @user.aaction == true && @user.comedy == true && @user.romance == true
-		special = actions & comedies & romances
-		@recom = special.select {|movie| movie.minutes < @user.flight.minutes }
-
-		# @recom is an array of movies that each fit the flight time.  I need to try every
-		# combination of those movies and add up the minutes and find the combination that is closest to 
-		# the flight time without going over.
-
-
-		@recom.each do |movie|
-			p movie.title
-			p movie.minutes
-		end
-
 		def get_combos(array)
 			n = array.length - 1
 			final_array = []
@@ -50,9 +23,6 @@ def update
 			end
 			final_array
 		end
-
-		array = @recom
-		p get_combos(array)
 
 		def add_array(array_of_arrays)
 			p array_of_arrays
@@ -69,50 +39,55 @@ def update
 					index_of_winner = index
 				end
 			end
-			p "*" * 100
-			p index_of_winner
-			p top_sum
 			@movie_fit = array_of_arrays[index_of_winner]
 		end
 
-		add_array(get_combos(array))
+		def get_movie_fit(special)
+			@recom = special.select {|movie| movie.minutes < @user.flight.minutes }
+			add_array(get_combos(@recom))
+		end
 
 
+	@user = User.find(params[:id])
+	@flights = Flight.all
 
+	@user.flight_id = params[:user][:flight_id]
+	@user.aaction = params[:aaction]
+	@user.romance = params[:romance]
+	@user.comedy = params[:comedy]
+	@user.save
 
+	actions = Genre.find(1).movies
+	comedies = Genre.find(2).movies
+	romances = Genre.find(3).movies
 
-
-
-
-
-
-
-
+	if @user.aaction == true && @user.comedy == true && @user.romance == true
+		special = actions & comedies & romances
+		get_movie_fit(special)
 
 	elsif @user.aaction == true && @user.comedy == true && @user.romance == nil
 		special = actions - romances & comedies
-		@recom = special.select {|movie| movie.minutes < @user.flight.minutes }
+		get_movie_fit(special)
 
 	elsif @user.aaction == nil && @user.comedy == true && @user.romance == true
 		special = comedies - actions & romances
-		@recom = special.select {|movie| movie.minutes < @user.flight.minutes }
+		get_movie_fit(special)
 
 	elsif @user.aaction == true && @user.comedy == nil && @user.romance == true
 		special = actions - comedies & romances
-		@recom = special.select {|movie| movie.minutes < @user.flight.minutes }
+		get_movie_fit(special)
 
 	elsif @user.aaction == true && @user.comedy == nil && @user.romance == nil
 		special = actions - comedies - romances
-		@recom = special.select {|movie| movie.minutes < @user.flight.minutes }
+		get_movie_fit(special)
 
 	elsif @user.aaction == nil && @user.comedy == true && @user.romance == nil
 		special = comedies - actions - romances
-		@recom = special.select {|movie| movie.minutes < @user.flight.minutes }
+		get_movie_fit(special)
 
 	elsif @user.aaction == nil && @user.comedy == nil && @user.romance == true
 		special = romances - actions - comedies
-		@recom = special.select {|movie| movie.minutes < @user.flight.minutes }
-
+		get_movie_fit(special)
 	end
 
 	@flight = @user.flight
