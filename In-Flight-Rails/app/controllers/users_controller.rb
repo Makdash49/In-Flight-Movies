c7771cd5967e1b943623df42749d7ba9ef19ac6c
Mark McQuillen <markdmcq@hotmail.com>
2015-07-27 17:00:56 -0700

    refactor users_controller with UsersHelper module

--- In-Flight-Rails/app/controllers/users_controller.rb
+++ In-Flight-Rails/app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+	include UsersHelper
+
 	def new
 		@user = User.new
 		@flights = Flight.all
@@ -14,40 +16,6 @@ class UsersController < ApplicationController
 
 
 def update
-		def get_combos(array)
-			n = array.length - 1
-			final_array = []
-			while n > 0
-				final_array += array.combination(n).to_a
-				n -= 1
-			end
-			final_array
-		end
-
-		def add_array(array_of_arrays)
-			p array_of_arrays
-			index_of_winner = nil
-			top_sum = 0
-			array_of_arrays.each_with_index do |array, index|
-			sum = 0
-				array.each do |movie|
-					sum += movie.minutes
-				end
-
-				if sum > top_sum && sum < @user.flight.minutes
-					top_sum = sum
-					index_of_winner = index
-				end
-			end
-			@movie_fit = array_of_arrays[index_of_winner]
-		end
-
-		def get_movie_fit(special)
-			@recom = special.select {|movie| movie.minutes < @user.flight.minutes }
-			add_array(get_combos(@recom))
-		end
-
-
 	@user = User.find(params[:id])
 	@flights = Flight.all
 
@@ -57,38 +25,11 @@ def update
 	@user.comedy = params[:comedy]
 	@user.save
 
-	actions = Genre.find(1).movies
-	comedies = Genre.find(2).movies
-	romances = Genre.find(3).movies
+	@actions = Genre.find(1).movies
+	@comedies = Genre.find(2).movies
+	@romances = Genre.find(3).movies
 
-	if @user.aaction == true && @user.comedy == true && @user.romance == true
-		special = actions & comedies & romances
-		get_movie_fit(special)
-
-	elsif @user.aaction == true && @user.comedy == true && @user.romance == nil
-		special = actions - romances & comedies
-		get_movie_fit(special)
-
-	elsif @user.aaction == nil && @user.comedy == true && @user.romance == true
-		special = comedies - actions & romances
-		get_movie_fit(special)
-
-	elsif @user.aaction == true && @user.comedy == nil && @user.romance == true
-		special = actions - comedies & romances
-		get_movie_fit(special)
-
-	elsif @user.aaction == true && @user.comedy == nil && @user.romance == nil
-		special = actions - comedies - romances
-		get_movie_fit(special)
-
-	elsif @user.aaction == nil && @user.comedy == true && @user.romance == nil
-		special = comedies - actions - romances
-		get_movie_fit(special)
-
-	elsif @user.aaction == nil && @user.comedy == nil && @user.romance == true
-		special = romances - actions - comedies
-		get_movie_fit(special)
-	end
+	evaluate_check_boxes
 
 	@flight = @user.flight
 	render 'new'
--- In-Flight-Rails/app/helpers/users_helper.rb
+++ In-Flight-Rails/app/helpers/users_helper.rb
@@ -1,2 +1,70 @@
 module UsersHelper
+
+	def get_combos(array)
+		n = array.length - 1
+		final_array = []
+		while n > 0
+			final_array += array.combination(n).to_a
+			n -= 1
+		end
+		final_array
+	end
+
+	def add_array(array_of_arrays)
+		p array_of_arrays
+		index_of_winner = nil
+		top_sum = 0
+		array_of_arrays.each_with_index do |array, index|
+		sum = 0
+			array.each do |movie|
+				sum += movie.minutes
+			end
+
+			if sum > top_sum && sum < @user.flight.minutes
+				top_sum = sum
+				index_of_winner = index
+			end
+		end
+		if index_of_winner != nil
+			@movie_fit = array_of_arrays[index_of_winner]
+		end
+	end
+
+	def get_movie_fit(special)
+		@recom = special.select {|movie| movie.minutes < @user.flight.minutes }
+		add_array(get_combos(@recom))
+	end
+
+
+	def evaluate_check_boxes
+		if @user.aaction == true && @user.comedy == true && @user.romance == true
+			special = @actions & @comedies & @romances
+			get_movie_fit(special)
+
+		elsif @user.aaction == true && @user.comedy == true && @user.romance == nil
+			special = @actions - @romances & @comedies
+			get_movie_fit(special)
+
+		elsif @user.aaction == nil && @user.comedy == true && @user.romance == true
+			special = @comedies - @actions & @romances
+			get_movie_fit(special)
+
+		elsif @user.aaction == true && @user.comedy == nil && @user.romance == true
+			special = @actions - @comedies & @romances
+			get_movie_fit(special)
+
+		elsif @user.aaction == true && @user.comedy == nil && @user.romance == nil
+			special = @actions - @comedies - @romances
+			get_movie_fit(special)
+
+		elsif @user.aaction == nil && @user.comedy == true && @user.romance == nil
+			special = @comedies - @actions - @romances
+			get_movie_fit(special)
+
+		elsif @user.aaction == nil && @user.comedy == nil && @user.romance == true
+			special = @romances - @actions - @comedies
+			get_movie_fit(special)
+		end
+	end		
+
 end
